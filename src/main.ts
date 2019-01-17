b5e75da793235e8df69778093bbb8ab53696668d
Jared Parkinson <jared@parkinson.im>
2019-01-17 14:02:25 -0700

    Added commander to the template

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
+import * as commander from 'commander';
+// import * as fs from 'graceful-fs';
+import * as path from 'path';
+
+commander
+  .version('0.1.0', '-v, --version')
+  .option('-i, --input <cmd>', 'Input Source')
+  .option('-o, --output <cmd>', 'Output File')
+  .parse(process.argv);
 export class Main {
-  public async exec(): Promise<void> {
-    return new Promise<void>(
-      (
-        resolve: (resolveValue: void) => void,
-        reject: (rejectValue: void) => void,
-      ) => {
-        resolve(undefined);
-        reject(undefined);
-      },
-    );
+  public run(input: string): void {
+    console.log(path.resolve(input));
   }
 }
 
-async function exec() {
-  const main = new Main();
+const main = new Main();
+// console.log(commander);
 
-  await main.exec();
-}
-
-exec();
+main.run(commander.input);
