97dbf3c24f45a0bd01ac6f59aea9a44fa38b3d74
Jared Parkinson <jared@parkinson.im>
2019-01-19 07:21:37 -0700

    Working on link extractor

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,40 @@
 import * as commander from 'commander';
-// import * as fs from 'graceful-fs';
-import * as path from 'path';
+import { LinkExtractor } from './LinkExtractor';
 
 commander
   .version('0.1.0', '-v, --version')
-  .option('-i, --input <cmd>', 'Input Source')
+  .option('-f, --folder <cmd>', 'Folder')
   .option('-o, --output <cmd>', 'Output File')
+  .option('-u, --url <cmd>', 'Url')
   .parse(process.argv);
 export class Main {
-  public run(input: string): void {
-    console.log(path.resolve(input));
+  public run(
+    inputPath: string = '',
+    url: string = '',
+    outputPath: string,
+  ): void {
+    const linkExtractor = new LinkExtractor();
+    console.log(inputPath);
+
+    let output: string[] = [];
+    linkExtractor
+      .extactFromFiles(inputPath)
+      .then((value: string[]) => {
+        output = output.concat(value);
+        console.log(value);
+      })
+      .catch((value: string[]) => {
+        console.log(value);
+      });
+
+    output = output.concat(linkExtractor.extractFromUrl(url));
+
+    console.log(outputPath);
+    console.log(output);
   }
 }
 
 const main = new Main();
 // console.log(commander);
 
-main.run(commander.input);
+main.run(commander.folder, commander.url, commander.output);
